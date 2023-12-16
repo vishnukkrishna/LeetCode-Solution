53a631048f2fda25f45e3225a459f5252180cbaf
vishnukkrishna <vishnukkrishna1999@gmail.com>
2023-12-16 10:44:25 +0530

    leetcode 2418

---
+++ leetcode-2418.py
@@ -0,0 +1,17 @@
+def sortPeople(names, heights):
+    n = len(heights)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if heights[j] < heights[j + 1]:
+                heights[j], heights[j + 1] = heights[j + 1], heights[j]
+                names[j], names[j + 1] = names[j + 1], names[j]
+
+    return names
+    
+    
+# names = ["Mary","John","Emma"]
+# heights = [180,165,170]
+names = ["Alice","Bob","Bob"]
+heights = [155,185,150]
+
+print(sortPeople(names, heights))
